04c474bc9f41dc5ad7aa7c682952f0dadc904c79
ice-charon <antoniy.shumanov@leftclick.io>
2025-06-16 11:59:21 +0300

    almost feature complete

--- helpers/build.ts
+++ helpers/build.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import { compileX } from "../libs/";
 import { POOL_TYPES, preprocBuildContractsLocal } from "./helpers";
-import { defaultACoeff, defaultBCoeff, defaultBaseUSDRate, defautlCurvePT } from "../wrappers/Pool"
+import { defaultACoeff, defaultBCoeff, defaultBaseUSDRate, defautlCurveT } from "../wrappers/Pool"
 
 (async () => {
     let autocleanup = undefined;
@@ -19,7 +19,7 @@ import { defaultACoeff, defaultBCoeff, defaultBaseUSDRate, defautlCurvePT } from
             defaultExpACoeff: tp == "bonding_curve" ? defaultACoeff : undefined,
             defaultExpBCoeff: tp == "bonding_curve" ? defaultBCoeff : undefined,
             defaultBaseUSDRate: tp == "bonding_curve" ? defaultBaseUSDRate : undefined,
-            defaultCTokenForCurve: tp == "bonding_curve" ? defautlCurvePT : undefined,
+            defaultCTokenForCurve: tp == "bonding_curve" ? defautlCurveT : undefined,
         };
         preprocBuildContractsLocal(ops);
 
--- wrappers/Pool.ts
+++ wrappers/Pool.ts
@@ -888,16 +888,16 @@ export class PoolStable extends PoolBase {
 
 }
 
-export const defautlCurvePT = 80n;
+export const defautlCurveT = 80n;
 export const defaultACoeff = 1105000000000n;
 export const defaultBCoeff = 7056000000n;
 export const defaultBaseUSDRate = 7000000000000000n;
 
-export function bciPoolConfigToCell(config: PoolConfig & { expACoeff?: bigint, expBCoeff?: bigint, baseUSDRate?: bigint, ctokenToCurvePT?: bigint }): Cell {
+export function bciPoolConfigToCell(config: PoolConfig & { expACoeff?: bigint, expBCoeff?: bigint, baseUSDRate?: bigint, ctokenToCurveT?: bigint }): Cell {
     let expACoeff = config.expACoeff ?? defaultACoeff;
     let expBCoeff = config.expBCoeff ?? defaultBCoeff;
     let baseUSDRate = config.baseUSDRate ?? defaultBaseUSDRate;
-    let ctokenToCurvePT = config.ctokenToCurvePT ?? defautlCurvePT;
+    let ctokenToCurveT = config.ctokenToCurveT ?? defautlCurveT;
     return beginCell()
         .storeUint(1, 1)
         .storeCoins(config.leftReserve)
@@ -911,7 +911,7 @@ export function bciPoolConfigToCell(config: PoolConfig & { expACoeff?: bigint, e
         .storeUint(expACoeff, expACoeff.toString(2).length)
         .storeUint(expBCoeff, expBCoeff.toString(2).length)
         .storeUint(baseUSDRate, baseUSDRate.toString(2).length)
-        .storeUint(ctokenToCurvePT, 7)
+        .storeCoins(ctokenToCurveT)
         .storeRef(beginCell()
             .storeAddress(config.routerAddress)
             .storeAddress(config.leftWalletAddress)
@@ -937,7 +937,7 @@ export function poolBciStorageParser(src: Cell) {
         expACoeff: ds.loadUintBig(defaultACoeff.toString(2).length), // in tests only, it's dynamically
         expBCoeff: ds.loadUintBig(defaultBCoeff.toString(2).length), // in tests only, it's dynamically
         baseUSDRate: ds.loadUintBig(defaultBaseUSDRate.toString(2).length), // in tests only, it's dynamically
-        ctokenToCurveCT: ds.loadUintBig(7),
+        ctokenToCurveT: ds.loadCoins(),
         ...(() => {
             let ds_p = ds.loadRef().beginParse()
             return {
@@ -979,7 +979,7 @@ export class PoolBCI extends PoolBase {
             coefficientA: result.stack.readBigNumber(),
             coefficientB: result.stack.readBigNumber(),
             baseUSDRate: result.stack.readBigNumber(),
-            tokenCurvePT: result.stack.readBigNumber(),
+            tokenCurveT: result.stack.readBigNumber(),
         };
     }
 
@@ -1000,7 +1000,7 @@ export class PoolBCI extends PoolBase {
             coefficientA: defaultACoeff,
             coefficientB: defaultBCoeff,
             baseUSDRate: defaultBaseUSDRate,
-            tokenCurvePT: defautlCurvePT,
+            tokenCurveT: defautlCurveT,
         }
         try {
             data = await this.getPoolData(provider)
